83e5d453f490431d9e8af8ecec84aad7ab2d5250
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 18:52:56 +0100

    Add a solution to Problem 14

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem14.scala
@@ -0,0 +1,37 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+/**
+  * Duplicate the elements of a list.
+  *
+  * ==Example==
+  * {{{
+  * scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+  * res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+  * }}}
+  */
+object Problem14 {
+  def duplicateBuiltIn[A](list: Seq[A]): Seq[A] = list.flatMap(elem => List(elem, elem))
+
+  def duplicateFold[A](list: Seq[A]): Seq[A] =
+    list.foldRight(Seq.empty[A]) { (elem, duplicated) => elem +: elem +: duplicated }
+
+  def duplicateRecursive[A](list: Seq[A]): Seq[A] = {
+    def innerRecursion(list: Seq[A], duplicated: Seq[A]): Seq[A] =
+      list match {
+        case head +: tail => innerRecursion(tail, head +: head +: duplicated)
+        case _            => duplicated.reverse
+      }
+
+    innerRecursion(list, Seq.empty[A])
+  }
+
+  def duplicateBuffer[A](list: Seq[A]): Seq[A] = {
+    var buffer = collection.mutable.ListBuffer.empty[A]
+    list.foreach { elem =>
+      buffer += elem
+      buffer += elem
+    }
+
+    buffer.toList
+  }
+}
---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem14Spec.scala
@@ -0,0 +1,16 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem14._
+
+class Problem14Spec extends FlatSpec with Matchers {
+  it should "duplicate the elements of a list" in {
+    val list        = List('a, 'b, 'c, 'c, 'd)
+    val duplicated  = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+
+    duplicateBuiltIn(list)    shouldBe duplicated
+    duplicateFold(list)       shouldBe duplicated
+    duplicateRecursive(list)  shouldBe duplicated
+    duplicateBuffer(list)     shouldBe duplicated
+  }
+}
