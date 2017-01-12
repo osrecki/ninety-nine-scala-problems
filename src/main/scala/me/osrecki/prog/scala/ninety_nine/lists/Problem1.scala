9e65698fdb1cdcde336b5e3333b314a6674ae5a2
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 04:53:04 +0100

    Add a solution to Problem 1

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem1.scala
@@ -0,0 +1,20 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+/**
+  * Find the last element of a list.
+  *
+  * ==Example==
+  * {{{
+  * scala> last(List(1, 1, 2, 3, 5, 8))
+  * res0: Int = 8
+  * }}}
+  */
+object Problem1 {
+  def lastBuiltIn[A](list: Seq[A]): A = list.last
+
+  def lastRecursive[A](list: Seq[A]): A = list match {
+    case head :: Nil  => head
+    case _    :: tail => lastRecursive(tail)
+    case _            => throw new NoSuchElementException("Empty list.")
+  }
+}
