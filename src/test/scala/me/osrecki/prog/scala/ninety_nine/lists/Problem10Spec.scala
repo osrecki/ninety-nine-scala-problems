74d6d0ad171adf813517721032fdb7aed0caf679
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 09:17:01 +0100

    Add a solution to Problem 10

---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem10Spec.scala
@@ -0,0 +1,13 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem10._
+
+class Problem10Spec extends FlatSpec with Matchers {
+  it should "run-length encode a list" in {
+    val list    = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    val encoded = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+
+    encode(list) shouldBe encoded
+  }
+}
