6913c676f33e413aff86c97e347e76991d48f311
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 07:08:32 +0100

    Add a solution to Problem 6

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem6.scala
@@ -0,0 +1,22 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import scala.annotation.tailrec
+
+/**
+  * Find out whether a list is a palindrome.
+  *
+  * ==Example==
+  * {{{
+  * scala> isPalindrome(List(1, 2, 3, 2, 1))
+  * res0: Boolean = true
+  * }}}
+  */
+object Problem6 {
+  def isPalindrome[A](list: Seq[A]): Boolean = list == list.reverse
+
+  @tailrec
+  def isPalindromeRecursive[A](list: Seq[A]): Boolean = list match {
+    case head +: middle :+ last => head == last && isPalindromeRecursive(middle)
+    case _                      => true
+  }
+}
---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem6Spec.scala
@@ -0,0 +1,60 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem6._
+
+class Problem6Spec extends FlatSpec with Matchers {
+  it should "return true for List(1, 2, 3, 2, 1)" in {
+    val list = List(1, 2, 3, 2, 1)
+
+    isPalindrome(list)          shouldBe true
+    isPalindromeRecursive(list) shouldBe true
+  }
+
+  it should "return true for List(1, 2, 1)" in {
+    val list = List(1, 2, 1)
+
+    isPalindrome(list)          shouldBe true
+    isPalindromeRecursive(list) shouldBe true
+  }
+
+  it should "return true for List(1, 1)" in {
+    val list = List(1, 1)
+
+    isPalindrome(list)          shouldBe true
+    isPalindromeRecursive(list) shouldBe true
+  }
+
+  it should "return true for a singleton list" in {
+    val singleton = List(1)
+
+    isPalindrome(singleton)          shouldBe true
+    isPalindromeRecursive(singleton) shouldBe true
+  }
+
+  it should "return true for an empty list" in {
+    isPalindrome(Nil)          shouldBe true
+    isPalindromeRecursive(Nil) shouldBe true
+  }
+
+  it should "return false for List(1, 2)" in {
+    val list = 1 to 2
+
+    isPalindrome(list)          shouldBe false
+    isPalindromeRecursive(list) shouldBe false
+  }
+
+  it should "return false for List(1, 1, 2)" in {
+    val list = Seq(1, 1, 2)
+
+    isPalindrome(list)          shouldBe false
+    isPalindromeRecursive(list) shouldBe false
+  }
+
+  it should "return false for List(1, 2, 3, 4, 5)" in {
+    val list = 1 to 5
+
+    isPalindrome(list)          shouldBe false
+    isPalindromeRecursive(list) shouldBe false
+  }
+}
