d26089158fe25cc0db02091356c207fe7a04e128
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 07:27:54 +0100

    Add a solution to Problem 7

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem7.scala
@@ -0,0 +1,17 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+/**
+  * Flatten a nested list structure.
+  *
+  * ==Example==
+  * {{{
+  * scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+  * res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+  * }}}
+  */
+object Problem7 {
+  def flatten(list: Seq[Any]): Seq[Any] = list flatMap {
+    case elemList: Seq[_] => flatten(elemList)
+    case elem             => Seq(elem)
+  }
+}
---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem7Spec.scala
@@ -0,0 +1,14 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem7._
+
+class Problem7Spec extends FlatSpec with Matchers {
+  it should "flatten a list of Any" in {
+    val list1 = List(List(1, 1), 2, List(3, List(5, 8)))
+    flatten(list1) shouldBe List(1, 1, 2, 3, 5, 8)
+
+    val list2 = List("This", List("is", List("a", "very", List("nested", List("list", ".")))))
+    flatten(list2) shouldBe "This is a very nested list .".split(" ").toSeq
+  }
+}
