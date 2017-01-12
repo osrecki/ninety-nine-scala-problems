55370a60d5bf902dd89ad3b2b8a0e388a7946fc7
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 06:08:31 +0100

    Add a solution to Problem 3

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem3.scala
@@ -0,0 +1,21 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+/**
+  * Find the Kth element of a list.
+  * By convention, the first element in the list has index 0.
+  *
+  * ==Example==
+  * {{{
+  * scala> nth(2, List(1, 1, 2, 3, 5, 8))
+  * res0: Int = 2
+  * }}}
+  */
+object Problem3 {
+  def nthBuiltIn[A](k: Int, list: Seq[A]): A = list(k)
+
+  def nthRecursive[A](k: Int, list: Seq[A]): A = (k, list) match {
+    case (0, head :: _) => head
+    case (_, _ :: tail) => nthRecursive(k - 1, tail)
+    case _              => throw new IndexOutOfBoundsException
+  }
+}
