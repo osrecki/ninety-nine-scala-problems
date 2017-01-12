062dad124424a242f450c97e1a2885e1bcd340ae
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 06:19:34 +0100

    Add a solution to Problem 4

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem4.scala
@@ -0,0 +1,28 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import scala.annotation.tailrec
+
+/**
+  * Find the number of elements of a list.
+  *
+  * ==Example==
+  * {{{
+  * scala> length(List(1, 1, 2, 3, 5, 8))
+  * res0: Int = 6
+  * }}}
+  */
+object Problem4 {
+  def lengthBuiltIn[A](list: Seq[A]): Int = list.length
+
+  def lengthFold[A](list: Seq[A]): Int = list.foldLeft(0)((length, _) => length + 1)
+
+  def lengthRecursive[A](list: Seq[A]): Int = {
+    @tailrec
+    def innerRecursion(list: Seq[A], length: Int): Int = list match {
+      case _ :: tail  => innerRecursion(tail, length + 1)
+      case _          => length
+    }
+
+    innerRecursion(list, 0)
+  }
+}
