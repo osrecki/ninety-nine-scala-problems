936f6c7dd382c0cbd99f2b7207cbf411df108448
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-12 06:35:11 +0100

    Add a solution to Problem 5

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem5.scala
@@ -0,0 +1,28 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import scala.annotation.tailrec
+
+/**
+  * Reverse a list.
+  *
+  * ==Example==
+  * {{{
+  * scala> reverse(List(1, 1, 2, 3, 5, 8))
+  * res0: List[Int] = List(8, 5, 3, 2, 1, 1)
+  * }}}
+  */
+object Problem5 {
+  def reverseBuiltIn[A](list: Seq[A]): Seq[A] = list.reverse
+
+  def reverseFold[A](list: Seq[A]): Seq[A] = list.foldLeft(Seq.empty[A]) { (reversed, elem) => elem +: reversed }
+
+  def reverseRecursive[A](list: Seq[A]): Seq[A] = {
+    @tailrec
+    def innerRecursion(list: Seq[A], reversed: Seq[A]): Seq[A] = list match {
+      case head +: tail => innerRecursion(tail, head +: reversed)
+      case _            => reversed
+    }
+
+    innerRecursion(list, Seq.empty[A])
+  }
+}
---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem5Spec.scala
@@ -0,0 +1,23 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem5._
+
+class Problem5Spec extends FlatSpec with Matchers {
+  it should "reverse a list" in {
+    reverseBuiltIn(Nil)   shouldBe empty
+    reverseFold(Nil)      shouldBe empty
+    reverseRecursive(Nil) shouldBe empty
+
+    val singleton = List(1)
+    reverseBuiltIn(singleton)   shouldBe singleton
+    reverseFold(singleton)      shouldBe singleton
+    reverseRecursive(singleton) shouldBe singleton
+
+    val list = List(10, 44, -1, 0, -23, 100)
+    val reversed = List(100, -23, 0, -1, 44, 10)
+    reverseBuiltIn(list)    shouldBe reversed
+    reverseFold(list)       shouldBe reversed
+    reverseRecursive(list)  shouldBe reversed
+  }
+}
