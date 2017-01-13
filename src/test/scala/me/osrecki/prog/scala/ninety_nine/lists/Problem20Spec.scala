c9472aef04e0b34e76bec31ea27f92a5a2f3b0b5
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-13 05:34:23 +0100

    Add more tests for Problem 20

--- src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem20Spec.scala
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem20Spec.scala
@@ -4,9 +4,16 @@ import org.scalatest.{FlatSpec, Matchers}
 import Problem20._
 
 class Problem20Spec extends FlatSpec with Matchers {
-  it should "remove Kth element from a list" in {
-    val list = List('a, 'b, 'c, 'd)
+  val list = List('a, 'b, 'c, 'd)
 
-    removeAt(1, list) shouldBe (List('a, 'c, 'd),'b)
+  it should "remove Kth element from a list" in {
+    removeAt(0, list) shouldBe (List('b, 'c, 'd), 'a)
+    removeAt(1, list) shouldBe (List('a, 'c, 'd), 'b)
+    removeAt(3, list) shouldBe (List('a, 'b, 'c), 'd)
+  }
+
+  it should "throw a NoSuchElementException for K out of bounds" in {
+    a [NoSuchElementException] should be thrownBy removeAt(-1, list)
+    a [NoSuchElementException] should be thrownBy removeAt(4, list)
   }
 }
