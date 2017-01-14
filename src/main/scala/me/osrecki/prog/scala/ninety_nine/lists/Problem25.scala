a146b6fc06c33968a1584e6b351214fa65b16c1f
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-14 21:37:46 +0100

    Add a solution to Problem 25

---
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem25.scala
@@ -0,0 +1,32 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import scala.reflect.ClassTag
+
+/**
+  * Generate a random permutation of the elements of a list.
+  *
+  * ==Example==
+  * {{{
+  * scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+  * res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+  * }}}
+  */
+object Problem25 {
+  import Problem23.randomSelect
+
+  def randomPermute[A](list: Seq[A], seed: Long): Seq[A] = randomSelect(list.size, list, seed)
+
+  def randomPermuteArray[A: ClassTag](list: Seq[A], seed: Long): Seq[A] = {
+    val random = new util.Random(seed)
+    val array = list.toArray
+    for(i <- array.length - 1 to 1 by -1) {
+      val index = random.nextInt(i + 1)
+
+      val elem = array(index)
+      array(index) = array(i)
+      array(i) = elem
+    }
+
+    array.toList
+  }
+}
---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem25Spec.scala
@@ -0,0 +1,13 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem25._
+
+class Problem25Spec extends FlatSpec with Matchers {
+  it should "generate a random permutation of the elements of a list" in {
+    val list      = List('a, 'b, 'c, 'd, 'e, 'f)
+
+    randomPermute(list, seed = 1)       shouldBe List('f, 'c, 'a, 'b, 'e, 'd)
+    randomPermuteArray(list, seed = 1)  shouldBe List('e, 'c, 'a, 'b, 'f, 'd)
+  }
+}
