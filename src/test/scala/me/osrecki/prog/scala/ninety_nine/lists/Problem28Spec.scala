75e71609f38e30f8abb26bc5d70c8ef2eb07b8e2
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-01-14 22:07:18 +0100

    Add a solution to Problem 28

---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem28Spec.scala
@@ -0,0 +1,23 @@
+package me.osrecki.prog.scala.ninety_nine.lists
+
+import org.scalatest.{FlatSpec, Matchers}
+import Problem28._
+
+class Problem28Spec extends FlatSpec with Matchers {
+  val lists = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e),
+    List('i, 'j, 'k, 'l), List('m, 'n), List('o))
+
+  it should "sort a list of lists according to the length of sublists" in {
+    val sorted = List(List('o), List('d, 'e), List('d, 'e), List('m, 'n),
+      List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l))
+
+    lsort(lists) shouldBe sorted
+  }
+
+  it should "sort a list of lists according to the length frequency of sublists" in {
+    val sorted = List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c),
+                      List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n))
+
+    lsortFreq(lists) shouldBe sorted
+  }
+}
