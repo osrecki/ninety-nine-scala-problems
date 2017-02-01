cc3e37ea26c366efd926e7d44750811667380650
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-02-01 23:39:39 +0100

    Add the soulution to the Problem 33

--- src/main/scala/me/osrecki/prog/scala/ninety_nine/arithmetic/S99Int.scala
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/arithmetic/S99Int.scala
@@ -5,6 +5,7 @@ class S99Int(val number: Int) {
 
   /**
     * Determine whether a given integer number is prime.
+    *
     * ==Example==
     * {{{
     * scala> 7.isPrime
@@ -14,6 +15,18 @@ class S99Int(val number: Int) {
   def isPrime: Boolean = {
     (number > 1) && primes.takeWhile(_ <= Math.sqrt(number)).forall(number % _ != 0)
   }
+
+  /**
+    * Determine whether two positive integer numbers are coprime.
+    * Two numbers are coprime if their greatest common divisor equals 1.
+    *
+    * ==Example==
+    * {{{
+    * scala> 35.isCoprimeTo(64)
+    * res0: Boolean = true
+    * }}}
+    */
+  def isComprimeTo(other: Int): Boolean = gcd(number, other) == 1
 }
 
 object S99Int {
@@ -24,6 +37,8 @@ object S99Int {
   /**
     * Determine the greatest common divisor of two positive integer numbers.
     * Use Euclid's algorithm.
+    *
+    * ==Example==
     * {{{
     * scala> gcd(36, 63)
     * res0: Int = 9
---
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/arithmetic/Problem33Spec.scala
@@ -0,0 +1,26 @@
+package me.osrecki.prog.scala.ninety_nine.arithmetic
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.Inspectors._
+import S99Int.intToS99Int
+
+class Problem33Spec extends FlatSpec with Matchers {
+  it should "return true for two comprime integers" in {
+    val coprimes = Seq((1, 10), (3, 5), (13, 17), (2, 3), (8, 9), (25, 36), (35, 64), (48, 65))
+
+    testCoprime(coprimes, expected = true)
+  }
+
+  it should "return false if two integers are not coprime" in {
+    val notCoprimes = Seq((2, 4), (3, 9), (10, 100), (20, 520), (1000, 100000))
+
+    testCoprime(notCoprimes, expected = false)
+  }
+
+  private def testCoprime(seq: Seq[(Int, Int)], expected: Boolean): Unit = {
+    forAll(seq) { t =>
+      t._1.isComprimeTo(t._2) shouldBe expected
+      t._2.isComprimeTo(t._1) shouldBe expected
+    }
+  }
+}
