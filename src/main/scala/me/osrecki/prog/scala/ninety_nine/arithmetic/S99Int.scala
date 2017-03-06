1ea318328672d4ac6d04253ccbab8abbd7ec6230
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-03-06 16:56:50 +0100

    Add the soulution to the Problem 37

--- src/main/scala/me/osrecki/prog/scala/ninety_nine/arithmetic/S99Int.scala
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/arithmetic/S99Int.scala
@@ -76,6 +76,22 @@ class S99Int(val number: Int) {
     */
   def primeFactorsMultiplicity: Map[Int, Int] =
     encode(primeFactors).map(_.swap).toMap
+
+  /**
+    * Calculate Euler's totient function phi(m) (improved).
+    * See problem P34 for the definition of Euler's totient function. If the
+    * list of the prime factors of a number m is known in the form of problem
+    * P36 then the function phi(m>) can be efficiently calculated as follows:
+    * Let ([p1, m1], [p2, m2], [p3, m3], ...) be the list of prime factors
+    * (and their multiplicities) of a given number m. Then phi(m) can be
+    * calculated with the following formula:
+    *
+    * phi(m) = (p1-1)*p1**(m1-1) * (p2-1)*p2**(m2-1) * (p3-1)*p3**(m3-1) * ...
+    *
+    * Note that a**b stands for the b-th power of a.
+    */
+  def totientImproved: Int =
+    primeFactorsMultiplicity.map { case (p, m) => (p - 1) * Math.pow(p, m - 1) }.product.toInt
 }
 
 object S99Int {
