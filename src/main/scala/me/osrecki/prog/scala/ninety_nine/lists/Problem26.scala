62628561a3c0402511c3ce558cbd40335e05ebc9
Dinko Osrecki <dinko.osrecki@gmail.com>
2017-07-11 00:04:32 +0200

    Add another solution and improve tests

--- src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem26.scala
+++ src/main/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem26.scala
@@ -1,14 +1,15 @@
 package me.osrecki.prog.scala.ninety_nine.lists
 
 /**
+ * ==Problem==
  * Generate the combinations of K distinct objects chosen from the N elements
- *  of a list.
+ * of a list.
  *
- *  ==Example==
- *  {{{
- *  scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
- *  res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
- *  }}}
+ * ==Example==
+ * {{{
+ * scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+ * res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
+ * }}}
  */
 object Problem26 {
   def sublists[A](list: Seq[A]): Seq[Seq[A]] = list match {
@@ -22,6 +23,59 @@ object Problem26 {
       sublists(list).flatMap { sublist =>
         combinations(k - 1, sublist.tail).map(sublist.head +: _)
       }
+    }
+  }
+
+  /**
+   * Generates all combinations of k elements within the list of size
+   * n in form of indices of the list in increasing order.
+   *
+   * ==Idea==
+   * Idea is to have a vector of size k keeping indices of elements in
+   * increasing order. First sequence is [n - k - 1, ..., n - 2, n - 1].
+   * The last sequence is [0, 1, ..., k - 1].
+   *
+   * In each iteration algorithm looks for the closest element to the
+   * start of the vector that can be decremented (i.e. greater than its
+   * index in the vector). This item is decremented and all the items
+   * from this element towards the beginning of the vector are filled
+   * with the values in decreasing order.
+   *
+   * ==Example==
+   * k = 3, n = 5
+   * [2, 3, 4] -> [1, 3, 4] -> [0, 3, 4] -> (0 done, decrement 3 and refill to the left)
+   * [1, 2, 4] -> [0, 2, 4] -> (0 done, decrement 2 and refill)
+   * [0, 1, 4] -> (0 done, 1 done, decrement 4 and refill)
+   * [1, 2, 3] -> [0, 2, 3] -> (0 done, decrement 2 and refill)
+   * [0, 1, 3] -> (0 done, 1 done, decrement 3 and refill)
+   * [0, 1, 2] -> (0 done, 1, done, 2 done, end)
+   */
+  def combinationsOfIndices(k: Int, n: Int): Seq[Vector[Int]] = {
+    def indicesRec(current: Vector[Int], combinations: Seq[Vector[Int]]): Seq[Vector[Int]] = {
+      val index = current.zipWithIndex.find { case (elem, i) => elem > i }.map(_._2)
+
+      index match {
+        case None => combinations
+        case Some(i) =>
+          val (left, right) = current.splitAt(i + 1)
+
+          val newLeft = Vector.range(current(i) - left.size, current(i))
+          val newCurrent = newLeft ++ right
+
+          indicesRec(newCurrent, newCurrent +: combinations)
+      }
+    }
+
+    val current = Vector.range(n - k, n)
+    indicesRec(current, Seq(current))
+  }
+
+  def combinations2[A](k: Int, list: Seq[A]): Seq[Seq[A]] = {
+    if (k > list.size) {
+      Seq.empty[Seq[A]]
+    } else {
+      val vector = list.to[Vector]
+      combinationsOfIndices(k, list.size).map { indices => indices.map(i => vector(i)) }
     }
   }
 }
--- src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem26Spec.scala
+++ src/test/scala/me/osrecki/prog/scala/ninety_nine/lists/Problem26Spec.scala
@@ -1,20 +1,46 @@
 package me.osrecki.prog.scala.ninety_nine.lists
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FunSpec, Matchers}
 import Problem26._
 
-class Problem26Spec extends FlatSpec with Matchers {
-  val list = List('a, 'b, 'c, 'd, 'e, 'f)
+class Problem26Spec extends FunSpec with Matchers {
+  val list = Seq('a, 'b, 'c, 'd, 'e)
+  val combinationsOfThree = Seq(
+    Seq('c, 'd, 'e), Seq('b, 'd, 'e), Seq('a, 'd, 'e), Seq('b, 'c, 'e), Seq('a, 'c, 'e),
+    Seq('a, 'b, 'e), Seq('b, 'c, 'd), Seq('a, 'c, 'd), Seq('a, 'b, 'd), Seq('a, 'b, 'c)
+  )
 
-  it should "generate empty list if k greater than list size" in {
-    combinations(7, list) shouldBe empty
+  describe("combinations") {
+    it("should generate empty list if k greater than list size") {
+      combinations(7, list) shouldBe empty
+    }
+
+    it("should generate combinations of one element") {
+      combinations(1, list) shouldBe list.map(Seq(_))
+    }
+
+    it("should generate the combinations of k distinct elements") {
+      val combs = combinations(3, list)
+
+      combs should have size 10
+      combs should contain theSameElementsAs combinationsOfThree
+    }
   }
 
-  it should "generate combinations of one element" in {
-    combinations(1, list) shouldBe list.map(List(_))
-  }
+  describe("combinations2") {
+    it("should generate empty list if k greater than list size") {
+      combinations2(7, list) shouldBe empty
+    }
 
-  it should "generate the combinations of k distinct elements" in {
-    combinations(3, list) should have size 20
+    it("should generate combinations of one element") {
+      combinations2(1, list) shouldBe list.map(Seq(_))
+    }
+
+    it("should generate the combinations of k distinct elements") {
+      val combs = combinations2(3, list)
+
+      combs should have size 10
+      combs should contain theSameElementsAs combinationsOfThree
+    }
   }
 }
